65600a54688969594029a505d521ec02d11d5cd4
sathya7336 <sathyaprakash.s@tigeranalytics.com>
2024-03-15 14:52:36 +0530

    Feature added

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,13 @@
 # Custom python functions
 
 def double_number(a):
-    return a+a
+    print(f'value before double_number(): {a}')
+    y=a+a
+    print(f'value after double_number(): {y}')
+    return y
 
 def square_number(a):
-    return a*a
+    y=a*a
+    print(f'value before square_number(): {a}')
+    print(f'value after square_number(): {y}')
+    return y
